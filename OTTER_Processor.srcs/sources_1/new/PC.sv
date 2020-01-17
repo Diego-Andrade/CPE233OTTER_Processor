04c557a12063c64f87b634f2f8963d8010f9309d
Diego Andrade <Bets636@gmail.com>
2020-01-17 11:54:34 -0800

    Adding documentation for PC.sv

--- OTTER_Processor.srcs/sources_1/new/PC.sv
+++ OTTER_Processor.srcs/sources_1/new/PC.sv
@@ -1,3 +1,22 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Created by: Diego Andrade & Brian Padilla
+// Create Date: 01/14/2020 11:00:00 AM
+// Design Name: Program Counter
+// Module Name: PC
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
 module PC(
     input logic PC_RST, PC_WRITE, 
     input logic [31:0] PC_DIN,
