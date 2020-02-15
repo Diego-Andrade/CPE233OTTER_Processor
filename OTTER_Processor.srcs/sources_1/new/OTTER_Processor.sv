dc69648e12e85f3c9fc46ce890abd7ea29f474c1
Diego Andrade <Bets636@gmail.com>
2020-02-15 13:55:03 -0800

    Connecting modules into processor

--- OTTER_Processor.srcs/sources_1/new/OTTER_Processor.sv
+++ OTTER_Processor.srcs/sources_1/new/OTTER_Processor.sv
@@ -21,26 +21,96 @@
 
 
 module OTTER_Processor(
-    input CLK,
-    // Temp data connections
-    input PC_RST, PC_WRITE,
-    input [1:0]PC_SOURCE,
-    input [31:0] JALR, BRANCH, JAL,
-    // End Temp
-    output [31:0] INSTRUCTION
+    input CLK
     );
     
-    logic [31:0] PC_COUNTER;
-    logic [31:0] tPC_DATA;
+  
+    logic tPC_RST, tPC_WRITE,                   // PC write enable
+          tRF_EN,                               // RF enable
+          tBR_EQ, tBR_LT, tBR_LTU,              // Branch cond gen
+          tALU_SRC_A, tALU_SRC_B;               // ALU source selects
+          
+    logic [1:0]  tPC_SOURCE,                    // PC source select
+                 tRF_WR_SEL;                    // RF write select
     
-    ProgRom prog_rom(.CLK(CLK), .ADDR(PC_COUNTER), .INSTRUCTION(INSTRUCTION));
+    logic [3:0] tALU_FUN;                       // ALU function select
     
-    Mux4 pc_mux(.SEL(PC_SOURCE), 
-                .ZERO(PC_COUNTER + 4), 
-                .ONE(JALR), 
-                .TWO(BRANCH), 
-                .THREE(JAL), 
-                .OUT(tPC_DATA));
-                
-    PC pc(.CLK(CLK), .PC_RST(PC_RST), .PC_WRITE(PC_WRITE), .PC_DIN(tPC_DATA));
+    logic [31:0] tPC_IN, tPC_OUT,               // PC data
+                 tJALR, tBRANCH, tJAL,          // Branch Address Generator
+                 tINSTRUCTION, tMEM_DOUT2,      // Memory data
+                 tRF_WD, tRF_RS1, tRF_RS2,      // Reg file data
+                 tALU_RESULT, tALU_A, tALU_B,   // ALU data, source in
+                 tCSR_REG,                      // Control Regs???
+                 tU, tI, tS, tJ, tB;            // Immed Gen data
+    PC pc(
+        .CLK(CLK), .PC_RST(tPC_RST), 
+        .PC_WRITE(tPC_WRITE), .PC_DIN(tPC_IN), 
+        .PC_COUNT(tPC_OUT));
+    
+    Mux4 pc_mux(
+        .SEL(tPC_SOURCE), 
+        .ZERO(tPC_OUT + 4), 
+        .ONE(tJALR), 
+        .TWO(tBRANCH), 
+        .THREE(tJAL), 
+        .OUT(tPC_IN));
+        
+    //ProgRom prog_rom(.CLK(CLK), .ADDR(PC_COUNTER), .INSTRUCTION(INSTRUCTION));
+    Memory mem(
+        .MEM_CLK(CLK), 
+        .MEM_ADDR1(tPC_OUT), .MEM_DOUT1(tINSTRUCTION),
+        .MEM_ADDR2(tALU_RESULT), .MEM_WD(tRF_RS2), .MEM_DOUT2(tMEM_DOUT2));
+    
+    RegFile regfile(
+        .CLK(CLK), 
+        .RF_EN(tRF_EN),
+        .RF_ADDR1(tINSTRUCTION[19:15]), .RF_RS1(tRF_RS1),
+        .RF_ADDR2(tINSTRUCTION[24:20]), .RF_RS2(tRF_RS2),
+        .RF_WA(tINSTRUCTION[11:7]), .RF_WD(tRF_WD));  
+    
+    Mux4 reg_mux(
+        .SEL(tRF_WR_SEL),
+        .ZERO(tPC_OUT),
+        .ONE(tCSR_REG),
+        .TWO(tMEM_DOUT2),
+        .THREE(tALU_RESULT),
+        .OUT(tRF_WD));      
+    
+    IMMED_GEN immed_gen(
+        .INSTRUCTION(tINSTRUCTION),
+        .U(tU), .I(tI), .S(tS), .J(tJ), .B(tB));
+        
+    BRANCH_ADDR_GEN branch_addr_gen(
+        .PC(tPC_OUT),
+        .J(tJ), .B(tB), .I(tI),
+        .RS1(tRF_RS1),
+        .JAL(tJAL), .BRANCH(tBRANCH), .JALR(tJALR));
+        
+    BRANCH_COND_GEN branch_cond_gen(
+        .RS1(tRF_RS1), .RS2(tRF_RS2),
+        .BR_EQ(tBR_EQ), .BR_LT(tBR_LT), .BR_LTU(tBR_LTU));
+        
+    ALU alu(
+        .A(tALU_A), .B(tALU_B),
+        .ALU_FUN(tALU_FUN),
+        .RESULT(tALU_RESULT)
+    );
+    
+    Mux2 alu_a_mux2(
+        .SEL(tALU_SRC_A),
+        .ZERO(tRF_RS1),
+        .ONE(tU),
+        .OUT(tALU_A)
+    );
+    
+    Mux4 alu_b_mux4(
+        .SEL(tALU_SRC_B),
+        .ZERO(tRF_RS2),
+        .ONE(tI),
+        .TWO(tS),
+        .THREE(tPC_OUT),
+        .OUT(tALU_B)
+    );
+    
+    
 endmodule
